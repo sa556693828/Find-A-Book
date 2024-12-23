760285d91acfaa371517088a13ae9c42cc2a9ba9
sa556693828 <sa556693828@gmail.com>
2024-12-23 22:59:56 +0800

    Refactor QueryBookRow component: Add image rendering with fallback icon and improve layout. Introduce 'unoptimized' attribute for images to enhance performance. Clean up commented-out code for better readability.

--- src/components/chating/QueryBookRow.tsx
+++ src/components/chating/QueryBookRow.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { BookData } from "@/types";
 import { HiDotsHorizontal } from "react-icons/hi";
 import { BsFillHandThumbsDownFill } from "react-icons/bs";
+import Image from "next/image";
+import { FaBook } from "react-icons/fa";
 // import { FaBook } from "react-icons/fa";
 // import Image from "next/image";
 
@@ -34,20 +36,35 @@ const QueryBookRow = ({ book, updateBookList }: QueryBookRowProps) => {
       onClick={() => {
         window.open(book.link, "_blank");
       }}
-      className="bg-[#D8D8D8] relative flex flex-col p-4 cursor-pointer hover:bg-[#b5b5b5] gap-4 transition-all duration-300 rounded-[4px]"
+      className="bg-[#D8D8D8] relative flex items-center p-4 cursor-pointer hover:bg-[#b5b5b5] gap-4 transition-all duration-300 rounded-[4px]"
     >
-      <div className="flex gap-1">
-        {book.keywords &&
-          book.keywords.length > 0 &&
-          book.keywords.map((keyword, index) => (
-            <div
-              key={index}
-              className="rounded-[4px] bg-[#CAC3BB] p-[6px] text-[12px]"
-            >
-              {keyword.split(" ").join("、")}
-            </div>
-          ))}
-        {/* {book.book_keywords &&
+      {book?.image ? (
+        <Image
+          src={book.image}
+          alt={book.title}
+          className="w-20 h-[110px]"
+          width={400}
+          height={310}
+          unoptimized
+        />
+      ) : (
+        <div className="w-20 h-[110px] bg-transparent flex items-center justify-center">
+          <FaBook className="size-4" color="#000000" />
+        </div>
+      )}
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-1">
+          {book.keywords &&
+            book.keywords.length > 0 &&
+            book.keywords.map((keyword, index) => (
+              <div
+                key={index}
+                className="rounded-[4px] bg-[#CAC3BB] p-[6px] text-[12px]"
+              >
+                {keyword.split(" ").join("、")}
+              </div>
+            ))}
+          {/* {book.book_keywords &&
             book.book_keywords.length > 0 &&
             book.book_keywords.map((keyword, index) => (
               <div
@@ -57,31 +74,32 @@ const QueryBookRow = ({ book, updateBookList }: QueryBookRowProps) => {
                 {keyword}
               </div>
             ))} */}
-      </div>
-      <p className="text-sm font-bold">{book.title}</p>
-      <div className="flex absolute top-2 right-2 items-center justify-center cursor-pointer menu-container">
-        <HiDotsHorizontal
-          className="size-3 text-[#969696] "
-          onClick={(e) => {
-            e.stopPropagation();
-            setActiveMenuId(activeMenuId === book.isbn ? null : book.isbn);
-          }}
-        />
-        {activeMenuId === book.isbn && (
-          <div className="absolute right-0 top-3 bg-[#E8E8E8] hover:bg-[#E8E8E8]/80 rounded-lg min-w-[120px] z-10">
-            <button
-              className="w-full px-3 py-2 text-black text-xs flex items-center justify-between gap-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                updateBookList(book.isbn);
-                setActiveMenuId(null);
-              }}
-            >
-              沒興趣
-              <BsFillHandThumbsDownFill className="size-3" color="" />
-            </button>
-          </div>
-        )}
+        </div>
+        <p className="text-sm font-bold">{book.title}</p>
+        <div className="flex absolute top-2 right-2 items-center justify-center cursor-pointer menu-container">
+          <HiDotsHorizontal
+            className="size-3 text-[#969696] "
+            onClick={(e) => {
+              e.stopPropagation();
+              setActiveMenuId(activeMenuId === book.isbn ? null : book.isbn);
+            }}
+          />
+          {activeMenuId === book.isbn && (
+            <div className="absolute right-0 top-3 bg-[#E8E8E8] hover:bg-[#E8E8E8]/80 rounded-lg min-w-[120px] z-10">
+              <button
+                className="w-full px-3 py-2 text-black text-xs flex items-center justify-between gap-2"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  updateBookList(book.isbn);
+                  setActiveMenuId(null);
+                }}
+              >
+                沒興趣
+                <BsFillHandThumbsDownFill className="size-3" color="" />
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
